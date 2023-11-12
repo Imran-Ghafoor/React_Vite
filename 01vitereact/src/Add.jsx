638e6c6376377a8014240412c77a84a955b12a4b
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-11-12 21:59:12 +0500

    basic react with vite

---
+++ 01vitereact/src/Add.jsx
@@ -0,0 +1,9 @@
+function Add() {
+  return (
+    <div>
+      <h2>Im React</h2>
+      <p>React is the javascript liberary!</p>
+    </div>
+  );
+}
+export default Add;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+import Add from "./Add";
+function App() {
+  return (
+    <div>
+      <h1>Hello Imran!</h1>
+      <Add />
+    </div>
+  );
+}
+
+export default App;
