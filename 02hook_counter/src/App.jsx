83fb9e44a78f940551163aa4d4f4725ecd8b9b67
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-11-12 22:37:43 +0500

    basic react hook with counter project

---
+++ 02hook_counter/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(0);
+  // let counter = 5
+  const addValue = () => {
+    if (counter < 20) {
+      // if value stop on 20
+
+      // counter = counter + 1;
+      setCounter(counter + 1);
+    }
+  };
+  const removeValue = () => {
+    if (counter > 0) {
+      counter = counter - 1;
+      setCounter(counter);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Hello Imran</h1>
+      <h2>Counter: {counter}</h2>
+      <button onClick={addValue}>Add Value:{counter} </button>
+      <br />
+      <button onClick={removeValue}>Remove Value:{counter} </button>
+      <footer>value {counter}:</footer>
+    </div>
+  );
+}
+
+export default App;
