8b361605a344e11e5d1d9e81442f7ee5e41b3e2d
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-11-14 23:51:30 +0500

    understand Tailwind css and Props

---
+++ 03tailwind_props/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
+        Hello Tailwind
+      </h1>
+      <Card username="Imran" btnText="Click me" />
+      <Card username="Khan" btnText="Visit me" />
+    </>
+  );
+}
+
+export default App;
