5766f79d1c13d88902b17c238a05937ada40099f
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-11-14 23:58:52 +0500

    tailwind and props

--- 03tailwind_props/src/App.jsx
+++ 03tailwind_props/src/App.jsx
@@ -8,7 +8,11 @@ function App() {
       <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
         Hello Tailwind
       </h1>
-      <Card username="Imran" btnText="Click me" />
+      <Card
+        username="Imran"
+        btnText="Click me"
+        paraText="HI, my self imran, im a enthuestic software developer"
+      />
       <Card username="Khan" btnText="Visit me" />
     </>
   );
--- 03tailwind_props/src/components/Card.jsx
+++ 03tailwind_props/src/components/Card.jsx
@@ -1,6 +1,6 @@
 // import React from "react";
 
-function Card({ username, btnText = "Visit me" }) {
+function Card({ username, paraText = "lorem", btnText = "Visit me" }) {
   console.log(username);
   return (
     <div className="relative h-[400px] w-[300px] rounded-md">
@@ -12,10 +12,7 @@ function Card({ username, btnText = "Visit me" }) {
       <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
       <div className="absolute bottom-4 left-4 text-left">
         <h1 className="text-lg font-semibold text-white">{username}</h1>
-        <p className="mt-2 text-sm text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-          debitis?
-        </p>
+        <p className="mt-2 text-sm text-gray-300">{paraText}</p>
         <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
           {btnText} →
         </button>
