dcb5723b8f0ed64bede892a8feb78118976cdd14
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-11-21 19:34:41 +0500

    React Router

--- 07ReactRouter/src/components/Github/Github.jsx
+++ 07ReactRouter/src/components/Github/Github.jsx
@@ -1,26 +1,38 @@
 import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 function Github() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("https://api.github.com/users/Imran-Ghafoor")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setData(data);
-      });
-  }, []);
+  const data = useLoaderData();
+
+  //   const [data, setData] = useState([]);
+  //   useEffect(() => {
+  //     fetch("https://api.github.com/users/Imran-Ghafoor")
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         console.log(data);
+  //         setData(data);
+  //       });
+  //   }, []);
 
   return (
-    <div className=" text-center bg-gray-600 m-8 p-4 text-white text-3xl ">
-       {data.name} <br />
-       {data.bio} <br />
-      
-      <div className="flex justify-center items-center pt-3 m-1 pb-1">
-        <img src={data.avatar_url} alt="Git picture" width={300} />
+    <div className=" text-center bg-gray-700 m-4 p-4 text-white text-3xl rounded-2xl ">
+      {data.name} <br />
+      {data.bio} <br />
+      <div className="flex justify-center items-center pt-4">
+        <img
+          className=" rounded-xl"
+          src={data.avatar_url}
+          alt="Git picture"
+          width={300}
+        />
       </div>
     </div>
   );
 }
 
 export default Github;
+
+export const githubInfoLoader = async () => {
+  const response = await fetch("https://api.github.com/users/Imran-Ghafoor");
+  return response.json();
+};
