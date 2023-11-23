ca0d88668f14d20770cebb43775689a533ecc138
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-11-24 01:48:04 +0500

    Todo Context api

--- 10TodoContextApiLocal/src/App.jsx
+++ 10TodoContextApiLocal/src/App.jsx
@@ -1,10 +1,48 @@
+import { useState } from "react";
 import "./App.css";
+import { TodoProvider } from "./Contexts/TodoContext";
 
+// context functinality
 function App() {
+  const [todos, setTodos] = useState([]);
+  const addTods = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+  };
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+  const toggleCompleted = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
+
+  // local storage functnality
   return (
-    <>
-      <h1 className=" bg-green-600 text-3xl">Hello</h1>
-    </>
+    <TodoProvider
+      value={{ Todos, addTodo, updateTodo, deleteTodo, toggleCompleted }}
+    >
+      <div className="bg-[#125ecf] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
   );
 }
 
