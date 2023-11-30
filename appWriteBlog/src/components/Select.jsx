77427dd21f34d8fc6bf706828abcc6f320b9a852
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2023-11-30 19:14:24 +0500

    Blog App

--- appWriteBlog/src/components/Select.jsx
+++ appWriteBlog/src/components/Select.jsx
@@ -1,19 +1,19 @@
-import React, { useId } from 'react'
+import React, { useId } from "react";
 
-function Select({
-    options,
-    label,
-    className,
-    ...props
-}, ref) {
-
-    const id = useId()
+function Select({ options, label, className, ...props }, ref) {
+  const id = useId();
 
   return (
-    <div className=' w-full'>
-        {label && <label htmlFor={id} className=''></label>}
+    <div className=" w-full">
+      {label && <label htmlFor={id} className=""></label>}
+      <select
+        {...props}
+        id={id}
+        ref={ref}
+        className={` px-3 gap-y-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+      ></select>
     </div>
-  )
+  );
 }
 
-export default Select
+export default Select;
